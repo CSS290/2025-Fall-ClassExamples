2d889fdbe771d4ec026ccc22545292266e17ff70
Kelvin Sung <ksung@uw.edu>
2025-10-14 18:14:22 -0700

    Update PointersAndReferences.cpp

--- wk4_10_13/2.PointersAndReferences/PointersAndReferences.cpp
+++ wk4_10_13/2.PointersAndReferences/PointersAndReferences.cpp
@@ -9,9 +9,10 @@ using namespace std;
 int main() {
     int aNum(10);
     int *p1(&aNum); // Pointer to aNum, can also initialze by: int *p1 = &aNum;
-    int &r = aNum; // r is a different name for bNum
-    int *ptr(&r); // Pointer initialized to r or bNum
+    int &r = aNum; // r is a different name for aNum
+    int *ptr(&r); // Pointer initialized to r or aNum
 
+    *p1 = 100;
     // 1. print aNum, &aNum, and p1
     cout << "aNum=" << aNum << "   &aNum=" << &aNum << endl;
     cout << " *p1=" << *p1 << "      p1=" << p1 <<  endl;
